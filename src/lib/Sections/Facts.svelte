e18ddf6faa71cde45e2e7bd7b1e6884b6f8d41af
Ezzoubair <98271602+zarqizoubir@users.noreply.github.com>
2023-07-04 15:06:40 +0100

    configure Facts section. split Facts section components.

--- src/lib/Sections/Facts.svelte
+++ src/lib/Sections/Facts.svelte
@@ -1,81 +1,23 @@
 <script>
+	import FactsItem from './../Widgets/Facts/FactsItem.svelte';
+	import FactsHeader from './../Widgets/Facts/FactsHeader.svelte';
+
+	const factsFeed = [
+		{ title: 'Happy Clients', headline: ' consequuntur quae', start: 0, end: 232 },
+		{ title: 'Projects', headline: ' adipisci atque cum quia aut', start: 0, end: 521 },
+		{ title: 'Hours of Support', headline: ' aut commodi quaerat', start: 0, end: 1453 },
+		{ title: 'Hard Workers', headline: ' rerum asperiores dolor', start: 0, end: 32 }
+	];
 </script>
 
 <section id="facts" class="facts">
 	<div class="container">
-		<div class="section-title">
-			<h2>Facts</h2>
-			<p>
-				Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem.
-				Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit
-				alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.
-			</p>
-		</div>
+		<FactsHeader />
 
 		<div class="row no-gutters">
-			<div class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch" data-aos="fade-up">
-				<div class="count-box">
-					<i class="bi bi-emoji-smile" />
-					<span
-						data-purecounter-start="0"
-						data-purecounter-end="232"
-						data-purecounter-duration="1"
-						class="purecounter"
-					/>
-					<p><strong>Happy Clients</strong> consequuntur quae</p>
-				</div>
-			</div>
-
-			<div
-				class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch"
-				data-aos="fade-up"
-				data-aos-delay="100"
-			>
-				<div class="count-box">
-					<i class="bi bi-journal-richtext" />
-					<span
-						data-purecounter-start="0"
-						data-purecounter-end="521"
-						data-purecounter-duration="1"
-						class="purecounter"
-					/>
-					<p><strong>Projects</strong> adipisci atque cum quia aut</p>
-				</div>
-			</div>
-
-			<div
-				class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch"
-				data-aos="fade-up"
-				data-aos-delay="200"
-			>
-				<div class="count-box">
-					<i class="bi bi-headset" />
-					<span
-						data-purecounter-start="0"
-						data-purecounter-end="1453"
-						data-purecounter-duration="1"
-						class="purecounter"
-					/>
-					<p><strong>Hours Of Support</strong> aut commodi quaerat</p>
-				</div>
-			</div>
-
-			<div
-				class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch"
-				data-aos="fade-up"
-				data-aos-delay="300"
-			>
-				<div class="count-box">
-					<i class="bi bi-people" />
-					<span
-						data-purecounter-start="0"
-						data-purecounter-end="32"
-						data-purecounter-duration="1"
-						class="purecounter"
-					/>
-					<p><strong>Hard Workers</strong> rerum asperiores dolor</p>
-				</div>
-			</div>
+			{#each factsFeed as feed}
+				<FactsItem {...feed} />
+			{/each}
 		</div>
 	</div>
 </section>
---
+++ src/lib/Widgets/Facts/FactsItem.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	export let start = 0;
+	export let end = 0;
+	export let duration = 1;
+	export let title: string;
+	export let headline: string;
+</script>
+
+<div
+	class="col-lg-3 col-md-6 d-md-flex align-items-md-stretch"
+	data-aos="fade-up"
+	data-aos-delay="100"
+>
+	<div class="count-box">
+		<i class="bi bi-journal-richtext" />
+		<slot />
+		<span
+			data-purecounter-start={start}
+			data-purecounter-end={end}
+			data-purecounter-duration={duration}
+			class="purecounter"
+		/>
+		<p><strong>{title}</strong>{headline}</p>
+	</div>
+</div>
