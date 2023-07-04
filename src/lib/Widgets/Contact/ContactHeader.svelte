0aa30f6ba2d580da1f60359f54c67d29c287b4c2
Ezzoubair <98271602+zarqizoubir@users.noreply.github.com>
2023-07-04 22:23:51 +0100

    testing .env

--- src/lib/Widgets/Contact/ContactHeader.svelte
+++ src/lib/Widgets/Contact/ContactHeader.svelte
@@ -1,11 +1,13 @@
 <script>
+	import { PUBLIC_SMTP } from '$env/static/public';
 </script>
 
 <div class="section-title">
 	<h2>Contact</h2>
 	<p>
+		{PUBLIC_SMTP}
 		Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit
-		sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias
-		ea. Quia fugiat sit in iste officiis commodi quidem hic quas.
+		sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea.
+		Quia fugiat sit in iste officiis commodi quidem hic quas.
 	</p>
 </div>
