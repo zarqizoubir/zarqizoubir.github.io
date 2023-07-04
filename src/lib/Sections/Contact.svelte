8e6fe881e4e1828db82c0ae668d4277b7a09efdf
Ezzoubair <98271602+zarqizoubir@users.noreply.github.com>
2023-07-04 14:53:54 +0100

    configure Contact section. split Contact section components.

--- src/lib/Sections/Contact.svelte
+++ src/lib/Sections/Contact.svelte
@@ -1,18 +1,12 @@
 <script>
+	import ContactHeader from '$lib/Widgets/Contact/ContactHeader.svelte';
 	import ContactForm from '$lib/Widgets/Contact/ContactForm.svelte';
 	import ContactInfo from '$lib/Widgets/Contact/ContactInfo.svelte';
 </script>
 
 <section id="contact" class="contact">
 	<div class="container">
-		<div class="section-title">
-			<h2>Contact</h2>
-			<p>
-				Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem.
-				Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit
-				alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.
-			</p>
-		</div>
+		<ContactHeader />
 
 		<div class="row" data-aos="fade-in">
 			<ContactInfo />
