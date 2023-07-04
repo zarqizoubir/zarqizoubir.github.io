793d4a3cbdc73578faae45419fd4d6c0a807dfbb
Ezzoubair <98271602+zarqizoubir@users.noreply.github.com>
2023-07-04 11:21:47 +0100

    new theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,33 +1,5 @@
-<script lang="ts">  
-	import SmallScreenNoti from '$lib/SmallScreenNoti.svelte';
-    import Navbar from '$lib/Navbar.svelte';
-	import SideNavigation from '$lib/SideNavigation.svelte';
-	import Navigation from '$lib/Navigation.svelte';
-    
-    import "../app.postcss";
-
+<script lang="ts">
+	import '../app.postcss';
 </script>
 
-<SmallScreenNoti/>
-
-<!-- <div class="container"><div class="outer-nav--return"></div>
-      <div id="viewport" class="l-viewport">
-        <div class="l-wrapper"> -->
-
-            <!-- </div>
-            </div>
-  </div> -->
-
-
-<div class="perspective effect-rotate-left">
-    <div class="container"><div class="outer-nav--return"></div>
-      <div id="viewport" class="l-viewport">
-        <div class="l-wrapper">
-          <Navbar/>
-          <SideNavigation/>
-          <slot/>
-        </div>
-      </div>
-    </div>
-    <Navigation/>
-</div>
+<slot />
