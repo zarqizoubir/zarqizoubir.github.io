d62cbd4263a1bb4567777e2719c720d43c3b35e6
Ezzoubair <98271602+zarqizoubir@users.noreply.github.com>
2023-07-10 01:16:49 +0100

    configure portfolio and projects

--- src/lib/Sections/Portfolio.svelte
+++ src/lib/Sections/Portfolio.svelte
@@ -2,23 +2,17 @@
 	import { PortfolioHeader, PortfolioFilter, PortfolioItem } from '$lib/Widgets/Portfolio';
 
 	const items = [
-		{ imgSrc: 'assets/img/portfolio/portfolio-1.jpg', href: '/', filter: 'app' },
-		{ imgSrc: 'assets/img/portfolio/portfolio-2.jpg', href: '/', filter: 'web' },
-		{ imgSrc: 'assets/img/portfolio/portfolio-3.jpg', href: '/', filter: 'app' },
-		{ imgSrc: 'assets/img/portfolio/portfolio-4.jpg', href: '/', filter: 'card' },
-		{ imgSrc: 'assets/img/portfolio/portfolio-5.jpg', href: '/', filter: 'web' },
-		{ imgSrc: 'assets/img/portfolio/portfolio-6.jpg', href: '/', filter: 'app' },
-		{ imgSrc: 'assets/img/portfolio/portfolio-7.jpg', href: '/', filter: 'card' },
-		{ imgSrc: 'assets/img/portfolio/portfolio-8.jpg', href: '/', filter: 'card' },
-		{ imgSrc: 'assets/img/portfolio/portfolio-9.jpg', href: '/', filter: 'web' }
+		{ imgSrc: 'projects_assets/django-docme.png', href: '/django-docme', filter: 'package' }
 	];
+
+	const filters = [{ name: 'Packages', filter: 'package' }];
 </script>
 
 <section id="portfolio" class="portfolio section-bg">
 	<div class="container">
 		<PortfolioHeader />
 
-		<PortfolioFilter />
+		<PortfolioFilter {filters} />
 
 		<div class="row portfolio-container" data-aos="fade-up" data-aos-delay="100">
 			{#each items as item}
--- src/lib/Widgets/Portfolio/PortfolioFilter.svelte
+++ src/lib/Widgets/Portfolio/PortfolioFilter.svelte
@@ -1,13 +1,14 @@
-<script>
+<script lang="ts">
+	export let filters: any;
 </script>
 
 <div class="row" data-aos="fade-up">
 	<div class="col-lg-12 d-flex justify-content-center">
 		<ul id="portfolio-flters">
 			<li data-filter="*" class="filter-active">All</li>
-			<li data-filter=".filter-app">App</li>
-			<li data-filter=".filter-card">Card</li>
-			<li data-filter=".filter-web">Web</li>
+			{#each filters as filter}
+				<li data-filter=".filter-{filter.filter}">{filter.name}</li>
+			{/each}
 		</ul>
 	</div>
 </div>
